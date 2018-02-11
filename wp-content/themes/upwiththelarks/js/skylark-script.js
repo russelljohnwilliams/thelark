2263d994461320c5831afb17759c9d985d1e20d7
russelljohnwilliams <russellwilliamscc@gmail.com>
2018-02-11 12:10:26 +0000

    tidy up css

--- wp-content/themes/upwiththelarks/js/skylark-script.js
+++ wp-content/themes/upwiththelarks/js/skylark-script.js
@@ -4,115 +4,102 @@
 var screenWidth = jQuery(window).width()
 var screenHeight = jQuery(window).height() -2
 var currentPosition = 0
-var resize
+// var resizeScreen
 var viewportWidth = jQuery('#viewport').width() 
 var images = []
 var viewportHeight
+var menuHeight
 
 jQuery( document ).ready(function() {
- cloneEvent()
- buildPages()
- getInstagramImage()
- // addHomeToMenu()
- setViewportMargin()
- setSiteHeader()
+ buildThePage()
+  // addHomeToMenu()
 })
 
-// resize function that deletes and redraws the size of the post tiles
+
+// resize function that deletes and redraws the post tiles
 
 jQuery(window).resize(function() {
   jQuery('#new-main').remove();
-  // jQuery('.post-wrapper').remove();
-  buildPages();
-  setPostSize()
-  setViewportMargin()
-  setSiteHeader()
+  buildThePage()
 })
 
-// pops Home/Maison to the beggining of the menu
-
-// function addHomeToMenu(){
-//   jQuery('.home-page').attr('id', "home")
-//   jQuery('.nav-menu').prepend('  <li class="homePage"><a href="#home">home | maison</a></li>')
-// }
+function buildThePage(){
+  showOpeningHours()
+  setViewportMargin()
+  sizeElements()
+  getInstagramImage()
+  
+  setSiteHeader()
+}
 
 
 // function that creates an animated scroll left or right through the pages inside the 'viewport' after a link is clicked in the header menu
 
 jQuery(function() {
-
   jQuery('.background-image').bind('click',function(event){
-var viewportPosition = jQuery('#viewport').offset().left;
-
-var anchor = jQuery(this).find('.anchor-point')[0].innerText.toLowerCase()
-var hashAnchor = "#" + anchor
-console.log("hash", hashAnchor)
-var targetAnchor = jQuery(hashAnchor).offset().left
-  
-var targetAnchorSum = targetAnchor - viewportPosition 
-var moveTo = targetAnchorSum + currentPosition
-jQuery('#viewport')
-.stop()
-.animate({
-  scrollLeft: (moveTo)
-}, 1000);
-currentPosition = moveTo
-event.preventDefault();
+    var viewportPosition = jQuery('#viewport').offset().left;
+    var anchor = jQuery(this).find('.anchor-point')[0].innerText.toLowerCase()
+    var hashAnchor = "#" + anchor
+    var targetAnchor = jQuery(hashAnchor).offset().left
+    var targetAnchorSum = targetAnchor - viewportPosition 
+    var moveTo = targetAnchorSum + currentPosition
+    jQuery('#viewport')
+    .stop()
+    .animate({
+      scrollLeft: (moveTo)
+    }, 1000);
+    currentPosition = moveTo
+    event.preventDefault();
   });
 });
 
 
-// returns any page back home
+// allows the link scroll any page back to home screen
 
 jQuery(function() {
-
   jQuery('.home-button').bind('click',function(event){
-
-var anchor = jQuery(this)[0].innerText.toLowerCase()
-var hashAnchor = "#" + anchor
-  
-var moveTo = 0
-jQuery('#viewport')
-.stop()
-.animate({
-  scrollLeft: (moveTo)
-}, 1000);
-currentPosition = moveTo
-event.preventDefault();
+    var anchor = jQuery(this)[0].innerText.toLowerCase()
+    var hashAnchor = "#" + anchor
+    var moveTo = 0
+    jQuery('#viewport')
+    .stop()
+    .animate({
+      scrollLeft: (moveTo)
+    }, 1000);
+    currentPosition = moveTo
+    event.preventDefault();
   });
 });
 
-// hide / show hidden elements / images in the tile posts
+
+// hide/show hidden elements/images in the tile posts
 
 jQuery(function(){
   jQuery('.background-hide').hide();
-
   jQuery('.background-image').hover(
     function(){
+      console.log("show")
       jQuery(this).children('.background-hide').stop(true, true).fadeIn()
       jQuery(this).children('.post-title').stop(true, true).fadeOut()
     },
     function(){
+      console.log("hide")
       jQuery(this).children('.background-hide').stop(true, true).fadeOut()
       jQuery(this).children('.post-title').stop(true, true).fadeIn() 
-
     });
 });
 
-jQuery(function(){
-  var menu = jQuery('.kix-paginateddocumentplugin')
-  menu.clone().appendTo('.food-clone')
-})
+
+// Pushes the main section of the screen below the header
 
 function setViewportMargin(){
-  var menuHeight = jQuery('#masthead').height()
+  menuHeight = jQuery('#masthead').height()
   jQuery('#viewport').css('margin-top', menuHeight + 30)
 }
 
-// a plugin is used to pull the most recent image from instagram, this strips away all the irrelevant html and presents just the image without
+// a plugin is used to pull the most recent image from instagram, this strips away all the irrelevant html and presents just the image. 
 
 function getInstagramImage(){
-
   setTimeout(function(){
     var instagramClone = jQuery('.instagram-clone')
     var instaImage = jQuery('div.hidden-instagram')
@@ -124,62 +111,32 @@ function getInstagramImage(){
     .appendTo(instagramClone) }, 2000);
 }
 
-// function cloneEvent(){
-//   var openingHoursContent = jQuery('div.hidden-opening-hours')
-//   .find(".simcal-event-details")
-//   .first()
-//   var eventContent = jQuery( "div.hidden-events")
-//   .find(".simcal-event-details")
-//   .first()
+// a plugin is used to grab the openng hours. If the skylark is open it will tell you how many hours till it is closed, if it is closed it will tell you when it opens.
 
-//   var clonedOpeningHours= jQuery('.cloned-opening-hours')
-//   var clonedEvent = jQuery('.cloned-event')
-
-//   openingHoursContent.clone()
-//   .appendTo(clonedOpeningHours)
-
-//   eventContent.clone()
-//   .appendTo(clonedEvent)
-// }
-
-
-function cloneEvent(){
-  var openingHoursContent = jQuery('div.hidden-opening-hours')
+function showOpeningHours(){
+  var todaysOpeningHoursContent = jQuery('div.hidden-opening-hours')
   .find(".simcal-event-details")
   .first()
-
   var tomorrowsOpeningHoursContent = jQuery('div.tomorrow')
   .find(".simcal-event-details")
   .first()
-
-  console.log("what it says", openingHoursContent.text().trim())
-
   var clonedOpeningHours = jQuery('.cloned-opening-hours')
-  if (openingHoursContent.text().trim() != "zzz" ){
-  var clone = openingHoursContent
-  // console.log("today", clone[0].innerText)
-}else{
-  var clone = tomorrowsOpeningHoursContent
-  // console.log("tomorrow", clone[0].innerText)
-}
-console.log("clone-thing", clone.text().trim())
-clone.clone()
-.appendTo(clonedOpeningHours)
+  if (todaysOpeningHoursContent.text().trim() != "zzz" ){
+    var clone = todaysOpeningHoursContent
+  }else{
+    var clone = tomorrowsOpeningHoursContent
+  }
+  clone.clone()
+  .appendTo(clonedOpeningHours)
 }
 
-
-
-
-
-
-function buildPages(){
-  console.log("poop poop")
+function sizeElements(){
   var headerHeight = jQuery('.site-header').height();
-  var screenHeight = jQuery(window).height() -2
+  // var screenHeight = jQuery(window).height() -2
   var viewportHeight = screenHeight - headerHeight;
-
   var newPosts = jQuery('.page-builder').eq(0)
-  viewportWidth = viewportHeight - 90 
+  viewportWidth = viewportHeight - menuHeight
+  console.log("MH", menuHeight)
   jQuery('#viewport').css('width', viewportWidth)
   setPostSize()
   jQuery("#main")
@@ -190,19 +147,19 @@ function buildPages(){
 }
 
 function setSiteHeader(){
-  screenWidth = jQuery(window).width()
+  // screenWidth = jQuery(window).width()
   jQuery('.site-header').css('width', viewportWidth)
   jQuery('#viewport').css('margin-left', (screenWidth - viewportWidth) / 2)
   jQuery('.site-header').css('margin-left', (screenWidth - viewportWidth) / 2)
 }
 
-function setPostSize(){
-
+// sets the size of each tile on the front page
 // MUST AMEND TO ALLOW FOR ADDITION OF BORDERS AND MARGINS
 
+function setPostSize(){
 var newPosts = jQuery('.page-builder').eq(0)
 viewportWidth = jQuery('#viewport').width()
-resize = (viewportWidth - 24) / 3 
+var resizeScreen = (viewportWidth - 24) / 3 
 if (screenWidth < 450){
   var postWrapper = jQuery('.post-wrapper')
   .css({"width": (viewportWidth - 2), "height": (screenHeight / 5)})
@@ -214,16 +171,45 @@ if (screenWidth < 450){
 else
 {
   var postWrapper = jQuery('.post-wrapper')
-  .css("width", (resize) )
+  .css("width", (resizeScreen) )
   .width();
   var border = jQuery('.post-wrapper').css("border")
   var totalWidth = (parseInt(border[0]) * 2)  + postWrapper
   jQuery('.post-wrapper').css('height', totalWidth +'px');
   jQuery('#viewport').height(viewportWidth)
 }
-  var viewportHeight = jQuery('#viewport').height()
-  jQuery('.page-builder').css('width', (viewportHeight) +'px')
-  jQuery('.page-wrapper').css('height', (viewportHeight) +'px')
-  jQuery('.page-builder').css('height', (viewportHeight) +'px')
+var viewportHeight = jQuery('#viewport').height()
+jQuery('.page-builder').css('width', (viewportHeight) +'px')
+jQuery('.page-wrapper').css('height', (viewportHeight) +'px')
+jQuery('.page-builder').css('height', (viewportHeight) +'px')
 }
+
+
+// function showOpeningHours(){
+//   var openingHoursContent = jQuery('div.hidden-opening-hours')
+//   .find(".simcal-event-details")
+//   .first()
+//   var eventContent = jQuery( "div.hidden-events")
+//   .find(".simcal-event-details")
+//   .first()
+//   var clonedOpeningHours= jQuery('.cloned-opening-hours')
+//   var clonedEvent = jQuery('.cloned-event')
+//   openingHoursContent.clone()
+//   .appendTo(clonedOpeningHours)
+//   eventContent.clone()
+//   .appendTo(clonedEvent)
+// }
+
+
+// pops Home/Maison to the beggining of the menu
+
+// function addHomeToMenu(){
+//   jQuery('.home-page').attr('id', "home")
+//   jQuery('.nav-menu').prepend('  <li class="homePage"><a href="#home">home | maison</a></li>')
+// }
+
+// jQuery(function(){
+//   var menu = jQuery('.kix-paginateddocumentplugin')
+//   menu.clone().appendTo('.food-clone')
+// })
 
